3dbd1ae4c7dd76710bb17e7a33e837aabb431203
Visrut Lukhi <100529711+Visrut2703@users.noreply.github.com>
2024-12-04 15:07:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',  // Make the server accessible externally
     port: 2346       // Specify the desired port (2345)
